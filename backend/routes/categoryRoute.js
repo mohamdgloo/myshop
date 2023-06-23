4393a32cec72d544e3156dad540363ade08071e9
Yahya Parvar <yahyaprvar1@gmail.com>
2023-06-23 19:29:48 +0330

    sex

--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,54 +1,52 @@
-import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Category from '../models/CategoryModel.js'
+import express from "express";
+import asyncHandler from "express-async-handler";
+import Category from "../models/CategoryModel.js";
 
-const router = express.Router()
+const router = express.Router();
 router.post(
-  '/createcategory',
-  asyncHandler(async (req, res) => { 
-        const cat = new Category({
-          category:req.body.category
-        })
-        cat.save().then(val=>{
-          res.json({msg:"add category",val:val})
-        })
-      })
-     
-      )
-      router.get(
-        '/category',
-        asyncHandler(async (req, res) => {
-          const cats = await Category.find({})
-          console.log(cats);
-          res.json(cats)
-        })
-      )
-        
-      //get one category
-      router.get(
-        '/:id',
-        asyncHandler(async(req,res)=>{
-          const getcat=await Category.findById(req.params.id)
-          res.json(getcat)
-          console.log(getcat);
-        })
-      )
+  "/createcategory",
+  asyncHandler(async (req, res) => {
+    const cat = new Category({
+      category: req.body.category,
+    });
+    cat.save().then((val) => {
+      res.json({ msg: "add category", val: val });
+    });
+  })
+);
+router.get(
+  "/category",
+  asyncHandler(async (req, res) => {
+    const cats = await Category.find({});
+    console.log(cats);
+    res.json(cats);
+  })
+);
 
+//get one category
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const getcat = await Category.findById(req.params.id);
+    const getSubByCat = await SubCategory.find({})
+      .where("category_id")
+      .equals(req.params.id)
+      .populate({ path: "category_id", select: "category -_id" });
+    const response = res.json({ getcat, getSubByCat });
+    console.log({ response });
+    return response;
+  })
+);
 
+// console.log(req);
+// const categoryName = req.category;
+// const CATEGORY = new Category({
+//     category:categoryName
+// })
+// CATEGORY.save(function (err) {
+//     if (err) console.log(err);
+//     // saved!
+//     res.send(true);
+// })
 
-
-    // console.log(req);
-    // const categoryName = req.category;
-    // const CATEGORY = new Category({
-    //     category:categoryName
-    // })
-    // CATEGORY.save(function (err) {
-    //     if (err) console.log(err);
-    //     // saved!
-    //     res.send(true);
-    // })
-
-
-
-export default router
-
+export default router;
--- frontend/src/components/category/CatAndSub.js
+++ frontend/src/components/category/CatAndSub.js
@@ -1,75 +1,80 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const CatAndSub = () => {
-    const[category,setCategory]=useState([])
-    const[showSub,setShowSub]=useState(false)
-    const[subCategory,setSubCategory]=useState([])
-   
-const handleClickForSub=(e)=>{
-       // setShowSub(!showSub)
-        console.log(subCategory);
-        console.log(e._id);
-}
-    useEffect(()=>{
-        const fetchCategory=async()=>{
-          const rescat=await axios.get('/api/category/category')
-          setCategory(rescat.data)
-        }
-        fetchCategory()
-      },[])
-      
+  const [category, setCategory] = useState([]);
+  const [showSub, setShowSub] = useState(false);
+  const [subCategory, setSubCategory] = useState([]);
 
-      useEffect(()=>{
-        const fetchSubCatWithCat=async()=>{
-          const resSubCatWithCat=await axios.get('api/subcategory/subcatbycategory:id')
-          setSubCategory(resSubCatWithCat.data)
-        }
-        fetchSubCatWithCat()
-      },[])
+  const handleClickForSub = (e) => {
+    // setShowSub(!showSub)
+    console.log(subCategory);
+    console.log(e._id);
+  };
+  useEffect(() => {
+    const fetchCategory = async () => {
+      const rescat = await axios.get("/api/category/category");
+      setCategory(rescat.data);
+    };
+    fetchCategory();
+  }, []);
 
+  const fetchSubCatWithCat = async (id) => {
+    const resSubCatWithCat = await axios.get(
+      `api/subcategory/subcatbycategory/${id}`
+    );
+    setSubCategory(resSubCatWithCat.data);
+  };
 
   return (
     <div>
-         <div className='rtl grid grid-cols-6 gap-2 my-5'>
-        {category.map((category,index)=>{
-          return(
-            <div key={index}  
-            className='border border-blue-300 rounded text-center text-blue-900 cursor-pointer'          
-            >                       
-                 <button 
-                   onClick={()=>{console.log(category._id);setShowSub(!showSub)}} >
-                     {category.category}
-                 </button>  
-                 
-                {showSub&&<div className='border border-blue-300 rounded w-3/3 h-auto'>
-                  {subCategory.map((subc,index)=>{
-                    if(subc.category_id===category.category){
-                      return(
-                        <p key={index}>{subc.subcategory}</p>
-                      )
-                    }else{return(null)}
-                  })}
-                </div>}
-            </div>
-          )
-        })}
-      </div>
-      <div className='rtl '>
-        {showSub&&
-                <div className='border border-blue-300 rounded w-3/3 h-auto'>
-                  {subCategory.map((subc,index)=>{
-                    if(subc.category_id===category._id){
-                      return(
-                        <p key={index}>{subc.subcategory}</p>
-                      )
-                    }else{return(null)}
+      <div className="rtl grid grid-cols-6 gap-2 my-5">
+        {category.map((category, index) => {
+          return (
+            <div
+              key={index}
+              className="border border-blue-300 rounded text-center text-blue-900 cursor-pointer"
+            >
+              <button
+                onClick={() => {
+                  console.log(category._id);
+                  setShowSub(!showSub);
+                  fetchSubCatWithCat(category._id);
+                }}
+              >
+                {category.category}
+              </button>
+
+              {showSub && (
+                <div className="border border-blue-300 rounded w-3/3 h-auto">
+                  {subCategory.map((subc, index) => {
+                    if (subc.category_id === category.category) {
+                      return <p key={index}>{subc.subcategory}</p>;
+                    } else {
+                      return null;
+                    }
                   })}
                 </div>
-         } 
-        </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className="rtl ">
+        {showSub && (
+          <div className="border border-blue-300 rounded w-3/3 h-auto">
+            {subCategory.map((subc, index) => {
+              if (subc.category_id === category._id) {
+                return <p key={index}>{subc.subcategory}</p>;
+              } else {
+                return null;
+              }
+            })}
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CatAndSub
+export default CatAndSub;
